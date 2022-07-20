7eb01d8c715a3ec3d51cce71dd9ef5938c47629c
Nikolay Afanasev <mcridednb@gmail.com>
2022-07-20 16:05:15 +0700

    fix db host

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "HOST": os.getenv("DB_HOST", "db"),
         "PORT": os.getenv("DB_PORT", 5432),
     }
 }
