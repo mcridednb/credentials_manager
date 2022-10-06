6325a8deda81207e905b0f9eb7e310687805ab82
Nikolay Afanasev <mcridednb@gmail.com>
2022-10-06 23:41:28 +0530

    fix

--- src/core/admin.py
+++ src/core/admin.py
@@ -290,7 +290,7 @@ class CredentialsProxyAdmin(admin.ModelAdmin):
                         if cookies is not None and isinstance(cookies, str):
                             cookies = json.loads(cookies)
 
-                        CredentialsProxy.objects.update_or_create(
+                        credentials_proxy, _, CredentialsProxy.objects.update_or_create(
                             credentials=credentials,
                             defaults={
                                 "proxy": proxy,
@@ -299,6 +299,8 @@ class CredentialsProxyAdmin(admin.ModelAdmin):
                                 "cookies": cookies,
                             }
                         )
+                        credentials_proxy.proxy = proxy
+                        credentials_proxy.save()
                     except IntegrityError:
                         pass
 
