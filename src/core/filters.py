802bb3a4ee33569ba0d0a1abb2c34de6a8251010
Nikolay Afanasev <mcridednb@gmail.com>
2022-11-01 16:34:17 +0400

    fix

--- src/core/filters.py
+++ src/core/filters.py
@@ -5,7 +5,7 @@ from core.models import CredentialsProxy
 
 class CredentialsFilter(django_filters.FilterSet):
     network = django_filters.CharFilter(
-        lookup_expr="credentials__network__title"
+        field_name="credentials__network__title"
     )
 
     class Meta:
